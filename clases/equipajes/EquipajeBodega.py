34616dc56a6786913d6ab9a49a33af550dc10aaf
Simon <simontigre2905@gmail.com>
2025-12-11 15:50:44 -0500

    .

--- clases/equipajes/EquipajeBodega.py
+++ clases/equipajes/EquipajeBodega.py
@@ -2,11 +2,11 @@ from abc import ABC, abstractmethod
 from typing import List, Dict, Any, Optional
 from datetime import datetime, date
 from clases.equipajes.Equipaje import Equipaje
+from clases.vuelos.Pasajero import Pasajero
 
 class EquipajeBodega(Equipaje):
-    def calcularCosto(self, claseVuelo: str) -> float:
-        if claseVuelo == "PREF" :  #and pasajero.equipajes.lenght == 0
+    def calcularCosto(self, claseVuelo: str, pasajero : Pasajero) -> float:
+        if claseVuelo == "PREF" and len(pasajero.__equipajes) == 0:
             costo = (self._peso * 10000) + (self._volumen * 5000)
             return costo
             
-        pass
